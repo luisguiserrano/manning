7068763ff012d4ceb8efaa5cf02e4ddae3ab45c3
Luis Serrano <luisgui.serrano@gmail.com>
2021-02-06 19:46:14 -0500

    Moving plots to utils.py

---
+++ Chapter 6 - Logistic Regression/utils.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib
+from matplotlib import pyplot
+
+# Some functions to plot our points and draw the lines
+def plot_points(features, labels):
+    X = np.array(features)
+    y = np.array(labels)
+    spam = X[np.argwhere(y==1)]
+    ham = X[np.argwhere(y==0)]
+    pyplot.scatter([s[0][0] for s in spam],
+                   [s[0][1] for s in spam],
+                   s = 100,
+                   color = 'cyan',
+                   edgecolor = 'k',
+                   marker = '^')
+    pyplot.scatter([s[0][0] for s in ham],
+                   [s[0][1] for s in ham],
+                   s = 100,
+                   color = 'red',
+                   edgecolor = 'k',
+                   marker = 's')
+    pyplot.xlabel('aack')
+    pyplot.ylabel('beep')
+    pyplot.legend(['happy','sad'])
+    
+def draw_line(a,b,c, color='black', linewidth=2.0, linestyle='solid', starting=0, ending=3):
+    # Plotting the line ax + by + c = 0
+    x = np.linspace(starting, ending, 1000)
+    pyplot.plot(x, -c/b - a*x/b, linestyle=linestyle, color=color, linewidth=linewidth)
