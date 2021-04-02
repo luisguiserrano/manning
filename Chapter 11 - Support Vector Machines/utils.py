4d1da0a2c122f6298f9ece59f79f6d538577de6b
Luis Serrano <luisgui.serrano@gmail.com>
2021-04-02 18:35:22 -0400

    Changing name of chapter 10 folder

--- Chapter 11 - Support Vector Machines/utils.py
+++ Chapter 11 - Support Vector Machines/utils.py
@@ -10,7 +10,7 @@ def plot_points(features, labels):
     ham = X[np.argwhere(y==0)]
     pyplot.scatter([s[0][0] for s in spam],
                    [s[0][1] for s in spam],
-                   s = 25,
+                   s = 35,
                    color = 'cyan',
                    edgecolor = 'k',
                    marker = '^')
@@ -27,14 +27,14 @@ def plot_points(features, labels):
 def plot_model(X, y, model):
     X = np.array(X)
     y = np.array(y)
-    plot_step = 0.2
+    plot_step = 0.01
     x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
     y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
                          np.arange(y_min, y_max, plot_step))
     Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
-    pyplot.contour(xx, yy, Z,colors = 'k',linewidths = 1)
+    pyplot.contour(xx, yy, Z,colors = 'k',linewidths = 3)
     plot_points(X, y)
     pyplot.contourf(xx, yy, Z, colors=['red', 'blue'], alpha=0.2, levels=range(-1,2))
     pyplot.show()
